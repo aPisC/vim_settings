fb44b7841aff02752d3e12d8cc8f6e142c5807e6
Bendeguz Cseresnyes <bendeguz.cseresnyes@hiya.com>
2022-12-15 14:14:47 +0100

    delete init.lua

--- init.lua
+++
@@ -1,9 +0,0 @@
--- Load all .lua files from ~/.config/nvim directory ordered by name 
--- (except: init.lua to avoid recutrion)
-
-local dir = "~/.config/nvim"
-for file in io.popen('find "'..dir..'" -maxdepth 1 -type f | sort | grep -v init.lua$ | grep .lua$'):lines() do
-  dofile(file)       
-end
-
-
